c16089b8f0238c21757e9046692287683972758e
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-09 09:39:33 +0100

    Create x-button/warning component

---
+++ app/components/x-button/warning.js
@@ -0,0 +1,11 @@
+import Component from '@ember/component';
+
+const WarningButton = Component.extend({
+  tagName: 'span'
+});
+
+WarningButton.reopenClass({
+  positionalParams: ["text"]
+})
+
+export default WarningButton;
