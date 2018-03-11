544a059d6fb830a422ef665e49fe72193e0658a9
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-11 01:32:14 +0100

    Prototype the registration

--- app/components/user-forms/join.js
+++ app/components/user-forms/join.js
@@ -3,37 +3,56 @@ import UserValidations from '../../validations/user';
 import Changeset from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 
-function isBlank(object){ // to refactor 
+function isBlank(object) {
+  // to refactor
   let isBlank = false;
-    [object.get('email'), object.get('password')].forEach((value) => {
-      if(value == ''){
-        isBlank = true;
-      }
-    })
-    return isBlank;
+  [object.get('email'), object.get('password')].forEach(value => {
+    if (value == '') {
+      isBlank = true;
+    }
+  });
+  return isBlank;
 }
 
 export default Component.extend({
   tagName: 'form',
-  init(){
+  init() {
     this._super(...arguments);
     let model = {
       email: '',
       password: '',
-      password_confirmation: ''
-    }
-    this.changeset = new Changeset(model, lookupValidator(UserValidations), UserValidations);    
+      password_confirmation: '',
+    };
+    this.changeset = new Changeset(
+      model,
+      lookupValidator(UserValidations),
+      UserValidations,
+    );
   },
   actions: {
-    join(changeset){
-      if(changeset.get('isValid') && !isBlank(changeset)){
-        console.log('We can register the user')
+    join(changeset) {
+      if (changeset.get('isValid') && !isBlank(changeset)) {
+        const user = {
+          email: changeset.get('email'),
+          password: changeset.get('password'),
+          password_registration: changeset.get('password_registration'),
+        };
+        fetch('http://localhost:3000/auth', {
+          body: JSON.stringify(user),
+          headers: {
+            'Accept': 'application/json',
+            'content-type': 'application/json'
+          },
+          method: 'POST', // *GET, PUT, DELETE, etc.
+        })
+        .then((response) => console.log(response))
+        .fetch((error) => console.error(error));
       } else {
-        console.log('Changeset is not valid, we cannot register the user')
+        console.log('Changeset is not valid, we cannot register the user');
       }
     },
-    clearForm(changeset){
+    clearForm(changeset) {
       return changeset.rollback();
-    }
-  }
+    },
+  },
 });
--- app/components/user-forms/login.js
+++ app/components/user-forms/login.js
@@ -3,37 +3,42 @@ import UserValidations from '../../validations/user';
 import Changeset from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 
-function isBlank(object){ // to refactor 
+function isBlank(object) {
+  // to refactor
   let isBlank = false;
-    [object.get('email'), object.get('password')].forEach((value) => {
-      if(value == ''){
-        isBlank = true;
-      }
-    })
-    return isBlank;
+  [object.get('email'), object.get('password')].forEach(value => {
+    if (value == '') {
+      isBlank = true;
+    }
+  });
+  return isBlank;
 }
 
 export default Component.extend({
   tagName: 'form',
-  init(){
+  init() {
     this._super(...arguments);
 
     let model = {
       email: '',
-      password: ''
-    }
-    this.changeset = new Changeset(model, lookupValidator(UserValidations), UserValidations);    
+      password: '',
+    };
+    this.changeset = new Changeset(
+      model,
+      lookupValidator(UserValidations),
+      UserValidations,
+    );
   },
   actions: {
-    authenticate(changeset){
-      if(changeset.get('isValid') && !isBlank(changeset)){  
-        console.log('Changeset is valid, we can authenticate')
+    authenticate(changeset) {
+      if (changeset.get('isValid') && !isBlank(changeset)) {
+        console.log('Changeset is valid, we can authenticate');
       } else {
         console.log('Changeset is not valid, no authentication');
       }
     },
-    clearForm(changeset){
+    clearForm(changeset) {
       return changeset.rollback();
-    }
-  }
+    },
+  },
 });
