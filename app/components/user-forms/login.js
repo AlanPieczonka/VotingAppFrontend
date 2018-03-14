b6692443dad6ce6634f8f025c7c5a073fbe56b22
AlanPieczonka <alanpieczonka@gmail.com>
2018-03-14 18:15:02 +0100

    Try to make authentication work

--- app/components/user-forms/login.js
+++ app/components/user-forms/login.js
@@ -3,11 +3,15 @@ import UserValidations from '../../validations/user';
 import Changeset from 'ember-changeset';
 import lookupValidator from 'ember-changeset-validations';
 
+import { inject as service } from '@ember/service';
 import { set } from '@ember/object';
 
 import { isAnyObjectValueBlank } from '../../javascript-helpers/validation';
 
 export default Component.extend({
+
+  session: service('session'),
+
   tagName: 'form',
   init() {
     this._super(...arguments);
@@ -26,6 +30,12 @@ export default Component.extend({
     authenticate(changeset) {
       if (changeset.get('isValid') && !isAnyObjectValueBlank(changeset, 'email', 'password')) {
         console.log('Changeset is valid, we can authenticate');
+
+        let identification = changeset.get('email'),
+            password = changeset.get('password');
+
+        this.get('session').authenticate('authenticator:devise', identification, password).catch(err => console.log(err));
+
       } else {
         set(this, 'responseMessage', 'Your data is not valid');
         set(this, 'isSuccess', false);
