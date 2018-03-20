09704cba60fc3acec711b5039f8f5473edc8a6fb
Alan Pieczonka <alanpieczonka@gmail.com>
2018-03-20 15:10:29 +0100

    Small code refactoring

--- app/routes/authenticated/user/edit.js
+++ app/routes/authenticated/user/edit.js
@@ -1,13 +1,11 @@
 import Route from '@ember/routing/route';
 
 import Changeset from 'ember-changeset';
-
 import UserValidations from '../../../validations/user';
 import lookupValidator from 'ember-changeset-validations';
 
 export default Route.extend({
-  // return model for upper route, remember to use changeset
-  model(){
+  model() {
     let model = this.modelFor('authenticated.user');
     return new Changeset(
       model,
