d1f86ac18819837fb7fa2df7e8cd735087e12c65
AlanPieczonka <alanpieczonka@gmail.com>
2018-03-30 23:43:05 +0200

    Display edit and deleteAccount buttons only for the authorized user

--- app/controllers/authenticated/user/index.js
+++ app/controllers/authenticated/user/index.js
@@ -1,9 +1,18 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { setFailureResponse } from '../../../javascript-helpers/responses';
 
 export default Controller.extend({
+  user: alias('model'),
   session: service(),
+  isAuthorized: computed('user.id', function() {
+      let userID = this.get('user').get('id'),
+          currentUserID = this.get('session.data.authenticated.account.data.id');
+
+    return userID == currentUserID ? true : false;
+  }),
   actions: {
     deleteAccount(model) {
       const data = localStorage.getItem('ember_simple_auth-session');
@@ -18,6 +27,6 @@ export default Controller.extend({
         .catch(() => {
           setFailureResponse.call(this);
         });
-    },
-  },
+    }
+  }
 });
