c1577176f4ccdfff4a149ec665919865c5d96f6b
AlanPieczonka <alanpieczonka@gmail.com>
2018-03-30 22:23:35 +0200

    Prevent user from accessing other user's edit page

--- app/routes/authenticated/user/edit.js
+++ app/routes/authenticated/user/edit.js
@@ -3,14 +3,24 @@ import Route from '@ember/routing/route';
 import Changeset from 'ember-changeset';
 import UserValidations from '../../../validations/user';
 import lookupValidator from 'ember-changeset-validations';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  session: service(),
   model() {
-    let model = this.modelFor('authenticated.user');
-    return new Changeset(
-      model,
-      lookupValidator(UserValidations),
-      UserValidations,
-    );
+    let model = this.modelFor('authenticated.user'),
+        modelID = model.get('id'),
+        currentUserID = this.get('session.data.authenticated.account.data.id')
+
+
+    if (modelID == currentUserID) {
+      return new Changeset(
+        model,
+        lookupValidator(UserValidations),
+        UserValidations
+      );
+    } else {
+      this.transitionTo('index');
+    }
   },
 });
