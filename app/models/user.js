6687e28e669c8f4312ec9787c7de74e5451f9a4c
AlanPieczonka <alanpieczonka@gmail.com>
2018-04-07 11:46:31 +0200

    Generate Poll model and display example polls on application's index template

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,13 @@
 import DS from 'ember-data';
 
-const { attr } = DS;
+const { attr, hasMany } = DS;
 
 export default DS.Model.extend({
   uid: attr('string'),
   email: attr('string'),
   name: attr('string'),
   nickname: attr('string'),
-  image: attr('string')
+  image: attr('string'),
+
+  polls: hasMany('poll')
 });
